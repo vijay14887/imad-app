02117cd76e050dee76847b22624f43c73c77e282
vijay14887 <vijay14887@gmail.com>
2017-08-04 16:56:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ var img = document.getElementById('img');
 var marginLeft = 0 ;
 function moveRight()
 {
-    marginLeft= marginLeft + 10;
+    marginLeft= marginLeft + 5;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function()
