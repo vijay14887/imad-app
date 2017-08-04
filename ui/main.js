71f533b3ce1fae1146849c9302bb56470783ea94
vijay14887 <vijay14887@gmail.com>
2017-08-04 16:27:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 
 var element = document.getElementById('maincontent');
 element.innerHTML = "New para";
+
+var img = document.getElementById('img');
+img.onclick = function()
+{
+    img.style.marginLeft = '100px';
+};
