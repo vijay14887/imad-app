c786566fa24f1a2982648d56090b70fe9b00e718
vijay14887 <vijay14887@gmail.com>
2017-08-04 10:18:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName', function(req, res) {
-    var articleName = req.parms.articleName;
+    var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
 
