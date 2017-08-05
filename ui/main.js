62746ffc51e1bf0407fc7b8ab0e2e8585adf15ed
vijay14887 <vijay14887@gmail.com>
2017-08-05 16:50:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,15 +15,28 @@ button.onclick = function() {
             span.innerHTML = counter.toString();
         }
         }
-    }
+    };
     
     // Make the request
     request.open('GET','http://vijay14887.imad.hasura-app.io/counter',true);
     request.send(null);
     
-}
+};
 
-
+var nameinput = document.getElementById('name').value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    // Make a request to the server and send the name
+    
+    // Capture the name list and render it as a list
+    var names = ['name1','name2','name3','name4'];
+    var list = '';
+    for (var i =0; i <names.length; i++){
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
 /* console.log('Loaded!');
 
 var element = document.getElementById('maincontent');
