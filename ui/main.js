e8fbe4fa8504619da66180a81ce2510a77895929
vijay14887 <vijay14887@gmail.com>
2017-08-05 13:10:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,29 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
+
+button.onclick = function() {
+    
+    // Create a request object
+    var request = XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE)
+        // take some action
+        if(request.status === 200){
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+    }
+    
+    // Make the request
+    request.open('GET','http://vijay14887.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+}
+
+
+/* console.log('Loaded!');
 
 var element = document.getElementById('maincontent');
 element.innerHTML = "New para";
@@ -14,4 +39,4 @@ function moveRight()
 img.onclick = function()
 {
    var interval=setInterval(moveRight,50);
-};
+};*/
