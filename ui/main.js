794305043276b0ae9c150394d6839f7f19642f0a
vijay14887 <vijay14887@gmail.com>
2017-08-07 10:09:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,7 +24,7 @@ var button = document.getElementById('counter');
     
 };*/
 
-
+var list = '';
 var submit = document.getElementById('submit_btn');
 
 submit.onclick = function() {
@@ -40,7 +40,7 @@ submit.onclick = function() {
            // var names = ['name1','name2','name3','name4'];
             var names = request.responseText;
             names = JSON.parse(names);
-            var list = '';
+            
             for (var i =0; i <names.length; i++){
             list += '<li>' + names[i] + '</li>';
             }
