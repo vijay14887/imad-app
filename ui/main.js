6d9628dd891b053c2d807c8ef120aad4d4b1b84b
vijay14887 <vijay14887@gmail.com>
2017-08-07 10:02:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 var button = document.getElementById('counter');
 
-button.onclick = function() {
+/* button.onclick = function() {
     
     // Create a request object
     var request = new XMLHttpRequest();
@@ -10,29 +10,49 @@ button.onclick = function() {
         if (request.readyState === XMLHttpRequest.DONE) {
         // take some action
         if(request.status === 200){
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
+            var names = ['name1','name2','name3','name4'];
+            var list = '';
+            for (var i =0; i <names.length; i++){
+            list += '<li>' + names[i] + '</li>';
+            }
         }
-        }
-    };
+    }
     
     // Make the request
-    request.open('GET','http://vijay14887.imad.hasura-app.io/counter',true);
+    request.open('GET','http://vijay14887.imad.hasura-app.io/submit-name?name=' + ,true);
     request.send(null);
     
-};
+};*/
 
-var nameinput = document.getElementById('name').value;
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
+
 submit.onclick = function() {
     // Make a request to the server and send the name
+      // Create a request object
+    var request = new XMLHttpRequest();
     
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+        // take some action
+        if(request.status === 200){
+           // var names = ['name1','name2','name3','name4'];
+            var names = request.responseText;
+            names = JSON.parse(names);
+            var list = '';
+            for (var i =0; i <names.length; i++){
+            list += '<li>' + names[i] + '</li>';
+            }
+        }
+    }
+    
+    // Make the request
+    request.open('GET','http://vijay14887.imad.hasura-app.io/submit-name?name=' + name ,true);
+    request.send(null);
     // Capture the name list and render it as a list
-    var names = ['name1','name2','name3','name4'];
-    var list = '';
-    for (var i =0; i <names.length; i++){
-        list += '<li>' + names[i] + '</li>';
+  
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
