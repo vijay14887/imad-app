ebb2cb9824d13a1249f4e10c60f3dfeab49eb0f6
vijay14887 <vijay14887@gmail.com>
2017-08-11 12:30:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -114,9 +114,22 @@ app.get('/submit-name', function(req, res) { //submit-name?name=xxxx
     res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName', function(req, res) {
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+app.get('/articles/:articleName', function(req, res) {
+  //  var articleName = req.params.articleName;
+  pool.query("SELECT * FROM article1 WHERE title = "+ req.params.articleName, function(err, result) {
+      if (err){
+          res.status(500).send(err.toString());
+      } else {
+          
+          if (result.rows.length === 0) {
+              res.status(404).send('Article Not found');
+          }
+            else{
+                var articleData = result.rows[0];
+                  res.send(createTemplate(articles[articleData]));
+            }
+      }
+  });
 });
 
 app.get('/ui/style.css', function (req, res) {
