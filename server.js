76870cfd87f35b2517b934fb96b7fe6f2f078208
vijay14887 <vijay14887@gmail.com>
2017-08-17 17:38:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -134,13 +134,13 @@ app.get('/articles/:articleName', function(req, res) {
 });
 
 function hash(input, salt){
-    var hashed = crypto.pbkdf2Sync(input, salt, 100000, 512, 'sha512');
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return hashed.toString('hex');
 }
 
 
 app.get('/hash/:input', function (req, res) {
-    var hashedString = hash(req.param.input,'this-is-some-random-string')
+    var hashedString = hash(req.params.input,'this-is-some-random-string')
     res.send(hashedString);
 });
 
