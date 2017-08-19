531db2a8860ad48d880ff25fcfad48083c14aed2
vijay14887 <vijay14887@gmail.com>
2017-08-19 12:38:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var config = {
     user: 'vijay14887',
@@ -16,6 +17,10 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+    secret: 'someRandomsecretvalue'
+    cookie: {maxage: 1000 * 60 * 60 * 24 * 30}
+}));
 
 /*var articles= {
     'article-one' : {
@@ -179,6 +184,12 @@ app.post('/login', function(req,res) {
                 var salt = dbString.split('$')[2];
                 var hashedPassword = hash(password,salt);
                 if( hashedPassword === dbString) {
+                    // set session
+                    req.session.auth = {userId: result.rows[0].id};
+                    // set cookie with session id
+                    // internally it maps the session id to an object on the server
+                    // { auth: {userId}}
+                    
                     res.send('credentials correct!');
                 } else {
                      res.send(403).send('username/password is invalid');
@@ -189,6 +200,14 @@ app.post('/login', function(req,res) {
    });
 });
 
+app.get('/check-login', function(req,res) {
+  if (req.session && req.session.auth && req.session.auth.userId) {
+      res.send('You are logged in: ' + req.session.auth.userId);
+  } else {
+       res.send('You are not logged in');
+  }
+      
+});
 
 
 app.get('/ui/style.css', function (req, res) {
