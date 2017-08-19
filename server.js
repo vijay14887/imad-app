5aef655dd78d38d145e23c46c0befc97b5640614
vijay14887 <vijay14887@gmail.com>
2017-08-19 13:12:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -209,6 +209,11 @@ app.get('/check-login', function(req,res) {
       
 });
 
+app.get('/logout', function(req,res)
+    delete req.session.auth;
+    res.send('You are logged out!')
+});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
